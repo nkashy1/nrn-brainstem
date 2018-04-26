5e7d8908fbb76aa152ed54194ac735d1f1a16eff
Neeraj Kashyap <neeraj@doc.ai>
2018-04-26 09:01:38 -0700

    Set up package.json and added Fuzz contract
    This will form the basis for the NRN token

---
+++ src/nrn.sol
@@ -0,0 +1,135 @@
+pragma solidity ^0.4.21;
+
+/**
+ * This contract is heavily inspired by the MiniMe contract:
+ * https://github.com/Giveth/minime/blob/master/contracts/MiniMeToken.sol
+ */
+
+contract Fuzz {
+    // Fuzz token is issued by a Fuzz Master, and this master has rights over
+    // the token in excess of regular users.
+    address public fuzzMaster;
+    
+    function hasMastery(address entity) internal view returns (bool) {
+        return entity == fuzzMaster;
+    }
+    
+    // Of course, the Fuzz Master can change. But only if the original one
+    // instigates the change.
+    function changeMastery(address newFuzzMaster) public {
+        require(hasMastery(msg.sender));
+        fuzzMaster = newFuzzMaster;
+    }
+    
+    // Display information for Fuzz token
+    // ERC20 methods: name, symbol
+    function name() public pure returns (string) {
+        return "Fuzz";
+    }
+    function symbol() public pure returns (string) {
+        return "FUZZ";
+    }
+    
+    // Ledger-related functionality
+    // ERC20 methods: balanceOf
+    mapping(address => uint256) ledger;
+    
+    function balanceOf(address entity) public view returns (uint256 balance) {
+        return ledger[entity];
+    }
+
+    // Supply-related functionality
+    // ERC20 methods: totalSupply
+    uint public totalSupply;
+    
+    function increaseSupply(uint256 amount) public {
+        require((msg.sender == address(this)) || hasMastery(msg.sender));
+        uint256 newSupply = totalSupply + amount;
+        require(newSupply > totalSupply);
+        totalSupply = newSupply;
+        ledger[msg.sender] += amount;
+    }
+    
+    function decreaseSupply(uint256 amount) public {
+        require((msg.sender == address(this)) || hasMastery(msg.sender));
+        require(ledger[msg.sender] >= amount);
+        uint256 newSupply = totalSupply - amount;
+        require(newSupply >= 0);
+        require(newSupply < totalSupply);
+        ledger[msg.sender] -= amount;
+        totalSupply = newSupply;
+    }
+
+    // Approval-related functionality
+    // ERC20 methods: approve, allowance
+    mapping(address => mapping(address => uint256)) allowances;
+    
+    function approve(address entity, uint256 allowance) public returns (bool success) {
+        require(allowance >= 0);
+        require((allowance == 0) || (allowances[msg.sender][entity] == 0));
+        allowances[msg.sender][entity] = allowance;
+        
+        Approval(msg.sender, entity, allowance);
+        
+        return true;
+    }
+    
+    function allowance(address owner, address proxy) public view returns (uint256) {
+        return allowances[owner][proxy];
+    }
+    
+    // Transfer-related functionality
+    // ERC20 methods: transfer, transferFrom
+    function _changeHands(address fromAddress, address toAddress, uint256 amount) internal returns (bool success) {
+        require(amount <= ledger[fromAddress]);
+        require((msg.sender == fromAddress) || (allowances[fromAddress][msg.sender] >= amount));
+        ledger[toAddress] += amount;
+        ledger[fromAddress] -= amount;
+        if (msg.sender != fromAddress) {
+            allowances[fromAddress][msg.sender] -= amount;
+        }
+        
+        emit Transfer(fromAddress, toAddress, amount);
+        
+        return true;
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        return _changeHands(msg.sender, _to, _value);
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        return _changeHands(_from, _to, _value);
+    }
+    
+    // ERC20 events: Transfer, Approval
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    
+    // Fuzz constructor
+    constructor(uint256 initialSupply) public {
+        fuzzMaster = msg.sender;
+        totalSupply = initialSupply;
+        ledger[msg.sender] = initialSupply;
+    }
+    
+    // Whitelist functionality to define which contracts balance can be reclaimed from
+    mapping(address => bool) reclamationWhitelist;
+    
+    function whitelistContractForReclamation(Fuzz oldFuzz) public returns (bool) {
+        require(hasMastery(msg.sender));
+        reclamationWhitelist[address(oldFuzz)] = true;
+        return true;
+    }
+    
+    // Allows transfer of token balance from old versions of the Fuzz contract
+    // to the current one. The current contract has to be approved to transfer
+    // at least the specified amount on behalf of the given account on the old contract.
+    function reclaimBalanceFrom(Fuzz oldFuzz, address account, uint256 amount) public returns (bool) {
+        require(reclamationWhitelist[address(oldFuzz)]);
+        require(oldFuzz.transferFrom(account, this, amount));
+        this.increaseSupply(amount);
+        require(this.transfer(account, amount));
+        return true;
+    }
+}
