bd880311a546ddcc4edb459737e655f9c7d9e22c
Neeraj Kashyap <neeraj@doc.ai>
2018-04-27 08:04:34 -0700

    Set required indentation to be 4 spaces

--- test/nrn.js
+++ test/nrn.js
@@ -30,18 +30,18 @@ const contractBytecode = _.get(compiledContract, 'bytecode');
  * parameters
  */
 function setUp(configuration) {
-  /* eslint-disable no-param-reassign */
-  configuration.provider = Ganache.provider();
-  configuration.accounts = _.get(configuration.provider, [
-    'manager',
-    'state',
-    'unlocked_accounts',
-  ]);
-  configuration.account_addresses = Object.keys(configuration.accounts);
-  configuration.web3 = new Web3();
-  configuration.web3.setProvider(configuration.provider);
-  configuration.Neuron = configuration.web3.eth.contract(JSON.parse(_.get(compiledContract, 'interface')));
-  /* eslint-enable no-param-reassign */
+    /* eslint-disable no-param-reassign */
+    configuration.provider = Ganache.provider();
+    configuration.accounts = _.get(configuration.provider, [
+        'manager',
+        'state',
+        'unlocked_accounts',
+    ]);
+    configuration.account_addresses = Object.keys(configuration.accounts);
+    configuration.web3 = new Web3();
+    configuration.web3.setProvider(configuration.provider);
+    configuration.Neuron = configuration.web3.eth.contract(JSON.parse(_.get(compiledContract, 'interface')));
+    /* eslint-enable no-param-reassign */
 }
 
 /**
@@ -56,247 +56,247 @@ function setUp(configuration) {
  * @param {...} rest - List of arguments to contract method, followed by callback
  */
 function getGasEstimateAndCall(
-  contractMethod,
-  fromAccount,
-  assignGas,
-  ...rest
+    contractMethod,
+    fromAccount,
+    assignGas,
+    ...rest
 ) {
-  const callback = rest[rest.length - 1];
-  const contractMethodArguments = rest.slice(0, rest.length - 1);
+    const callback = rest[rest.length - 1];
+    const contractMethodArguments = rest.slice(0, rest.length - 1);
 
-  return contractMethod.estimateGas(
-    ...contractMethodArguments,
-    (err, gasEstimate) => {
-      if (err) {
-        return callback(err);
-      }
-
-      return contractMethod(
+    return contractMethod.estimateGas(
         ...contractMethodArguments,
-        {
-          from: fromAccount,
-          gas: assignGas(gasEstimate),
+        (err, gasEstimate) => {
+            if (err) {
+                return callback(err);
+            }
+
+            return contractMethod(
+                ...contractMethodArguments,
+                {
+                    from: fromAccount,
+                    gas: assignGas(gasEstimate),
+                },
+                callback,
+            );
         },
-        callback,
-      );
-    },
-  );
+    );
 }
 
 describe('NRN compilation', () => {
-  it('should return no errors', (done) => {
-    const errors = _.get(compilationResult, 'errors', []);
-    assert.equal(errors.length, 0);
-    done();
-  });
+    it('should return no errors', (done) => {
+        const errors = _.get(compilationResult, 'errors', []);
+        assert.equal(errors.length, 0);
+        done();
+    });
 
-  it('should return no warnings', (done) => {
-    const warnings = _.get(compilationResult, 'warnings', []);
-    assert.equal(warnings.length, 0);
-    done();
-  });
+    it('should return no warnings', (done) => {
+        const warnings = _.get(compilationResult, 'warnings', []);
+        assert.equal(warnings.length, 0);
+        done();
+    });
 
-  it('should produce bytecode for the Neuron contract', (done) => {
-    assert(!!contractBytecode);
-    done();
-  });
+    it('should produce bytecode for the Neuron contract', (done) => {
+        assert(!!contractBytecode);
+        done();
+    });
 });
 
 describe('NRN construction', () => {
-  const configuration = {};
+    const configuration = {};
 
-  before(() => {
-    setUp(configuration);
-  });
+    before(() => {
+        setUp(configuration);
+    });
 
-  after((done) => {
-    configuration.provider.close(done);
-  });
+    after((done) => {
+        configuration.provider.close(done);
+    });
 
-  it('should be called with a name, symbol, and an initial token supply', (done) => {
-    configuration.web3.eth.estimateGas(
-      { data: contractBytecode },
-      (err, gasEstimate) => {
-        if (err) {
-          return done(err);
-        }
+    it('should be called with a name, symbol, and an initial token supply', (done) => {
+        configuration.web3.eth.estimateGas(
+            { data: contractBytecode },
+            (err, gasEstimate) => {
+                if (err) {
+                    return done(err);
+                }
 
-        // Some of the web3 contract methods (like new) make multiple calls to
-        // the provided callbacks. These callbacks represent different events.
-        // The `callInfo` object tracks the number of calls made to each
-        // callback.
-        const callInfo = {
-          new: 0,
-        };
+                // Some of the web3 contract methods (like new) make multiple calls to
+                // the provided callbacks. These callbacks represent different events.
+                // The `callInfo` object tracks the number of calls made to each
+                // callback.
+                const callInfo = {
+                    new: 0,
+                };
 
-        return configuration.Neuron.new(
-          'Neuron',
-          'NRN',
-          100,
-          {
-            from: configuration.account_addresses[0],
-            data: contractBytecode,
-            gas: 2 * gasEstimate,
-          },
+                return configuration.Neuron.new(
+                    'Neuron',
+                    'NRN',
+                    100,
+                    {
+                        from: configuration.account_addresses[0],
+                        data: contractBytecode,
+                        gas: 2 * gasEstimate,
+                    },
           (creationErr, contractInstance) => { // eslint-disable-line
-            if (creationErr) {
-              return done(creationErr);
-            }
+                        if (creationErr) {
+                            return done(creationErr);
+                        }
 
-            callInfo.new += 1;
+                        callInfo.new += 1;
 
-            if (callInfo.new === 2) {
-              return done();
-            }
-          },
+                        if (callInfo.new === 2) {
+                            return done();
+                        }
+                    },
+                );
+            },
         );
-      },
-    );
-  });
+    });
 
-  it('should raise an error if it is called with insufficient gas', done =>
-    configuration.web3.eth.estimateGas(
-      { data: contractBytecode },
-      (err, gasEstimate) => { // eslint-disable-line no-unused-vars
-        if (err) {
-          return done(err);
-        }
+    it('should raise an error if it is called with insufficient gas', done =>
+        configuration.web3.eth.estimateGas(
+            { data: contractBytecode },
+            (err, gasEstimate) => { // eslint-disable-line no-unused-vars
+                if (err) {
+                    return done(err);
+                }
 
-        return configuration.Neuron.new(
-          'Neuron',
-          'NRN',
-          100,
-          {
-            from: configuration.account_addresses[0],
-            data: contractBytecode,
-            gas: 1,
-          },
-          (creationErr, contractInstance) => { // eslint-disable-line no-unused-vars
-            if (creationErr) {
-              return done();
-            }
+                return configuration.Neuron.new(
+                    'Neuron',
+                    'NRN',
+                    100,
+                    {
+                        from: configuration.account_addresses[0],
+                        data: contractBytecode,
+                        gas: 1,
+                    },
+                    (creationErr, contractInstance) => { // eslint-disable-line no-unused-vars
+                        if (creationErr) {
+                            return done();
+                        }
 
-            return done(new Error('No error raised'));
-          },
-        );
-      },
-    ));
+                        return done(new Error('No error raised'));
+                    },
+                );
+            },
+        ));
 });
 
 describe('ERC20 methods', () => {
-  const configuration = {};
+    const configuration = {};
 
-  before((done) => {
-    setUp(configuration);
+    before((done) => {
+        setUp(configuration);
 
-    configuration.web3.eth.estimateGas(
-      { data: contractBytecode },
-      (err, gasEstimate) => {
-        if (err) {
-          return done(err);
-        }
+        configuration.web3.eth.estimateGas(
+            { data: contractBytecode },
+            (err, gasEstimate) => {
+                if (err) {
+                    return done(err);
+                }
 
-        // Some of the web3 contract methods (like new) make multiple calls to
-        // the provided callbacks. These callbacks represent different events.
-        // The `callInfo` object tracks the number of calls made to each
-        // callback.
-        const callInfo = {
-          new: 0,
-        };
+                // Some of the web3 contract methods (like new) make multiple calls to
+                // the provided callbacks. These callbacks represent different events.
+                // The `callInfo` object tracks the number of calls made to each
+                // callback.
+                const callInfo = {
+                    new: 0,
+                };
 
-        return configuration.Neuron.new(
-          'Neuron',
-          'NRN',
-          100,
-          {
-            from: configuration.account_addresses[0],
-            data: contractBytecode,
-            gas: 2 * gasEstimate,
-          },
-          (creationErr, contractInstance) => { // eslint-disable-line consistent-return
-            // eslint-disable-line consistent-return
-            if (creationErr) {
-              return done(creationErr);
-            }
+                return configuration.Neuron.new(
+                    'Neuron',
+                    'NRN',
+                    100,
+                    {
+                        from: configuration.account_addresses[0],
+                        data: contractBytecode,
+                        gas: 2 * gasEstimate,
+                    },
+                    (creationErr, contractInstance) => { // eslint-disable-line consistent-return
+                        // eslint-disable-line consistent-return
+                        if (creationErr) {
+                            return done(creationErr);
+                        }
 
-            callInfo.new += 1;
+                        callInfo.new += 1;
 
-            if (callInfo.new === 2) {
-              configuration.neuronInstance = contractInstance;
-              return done();
-            }
-          },
+                        if (callInfo.new === 2) {
+                            configuration.neuronInstance = contractInstance;
+                            return done();
+                        }
+                    },
+                );
+            },
         );
-      },
-    );
-  });
-
-  after((done) => {
-    configuration.provider.close(done);
-  });
-
-  it('testSupply should be callable by anyone and return the total amount of the token in circulation', (done) => {
-    configuration.neuronInstance.totalSupply.estimateGas((err, gasEstimate) => {
-      if (err) {
-        return done(err);
-      }
-
-      return configuration.neuronInstance.totalSupply(
-        {
-          from:
-            configuration.account_addresses[
-              configuration.account_addresses.length - 1
-            ],
-          gas: 2 * gasEstimate,
-        },
-        (supplyErr, supply) => {
-          if (supplyErr) {
-            return done(supplyErr);
-          }
-
-          assert.strictEqual(supply.toNumber(), 100);
-          return done();
-        },
-      );
     });
-  });
 
-  it('the name and symbol of the token should be visible to anyone', (done) => {
-    const methods = ['name', 'symbol'].map(k =>
-      async.apply(getGasEstimateAndCall, configuration.neuronInstance[k]));
-    return async.applyEach(
-      methods,
-      configuration.account_addresses[
-        configuration.account_addresses.length - 1
-      ],
-      gasEstimate => 2 * gasEstimate,
-      (err, results) => {
-        if (err) {
-          return done(err);
-        }
+    after((done) => {
+        configuration.provider.close(done);
+    });
 
-        assert.strictEqual(results[0], 'Neuron');
-        assert.strictEqual(results[1], 'NRN');
-        return done();
-      },
-    );
-  });
+    it('testSupply should be callable by anyone and return the total amount of the token in circulation', (done) => {
+        configuration.neuronInstance.totalSupply.estimateGas((err, gasEstimate) => {
+            if (err) {
+                return done(err);
+            }
 
-  it('the balance of any address should be visible to any other address', done =>
-    getGasEstimateAndCall(
-      configuration.neuronInstance.balanceOf,
-      configuration.account_addresses[
-        configuration.account_addresses.length - 1
-      ],
-      gasEstimate => 2 * gasEstimate,
-      configuration.account_addresses[0],
-      (err, balance) => {
-        if (err) {
-          return done(err);
-        }
+            return configuration.neuronInstance.totalSupply(
+                {
+                    from:
+            configuration.account_addresses[
+                configuration.account_addresses.length - 1
+            ],
+                    gas: 2 * gasEstimate,
+                },
+                (supplyErr, supply) => {
+                    if (supplyErr) {
+                        return done(supplyErr);
+                    }
 
-        assert.strictEqual(balance.toNumber(), 100);
-        return done();
-      },
-    ));
+                    assert.strictEqual(supply.toNumber(), 100);
+                    return done();
+                },
+            );
+        });
+    });
+
+    it('the name and symbol of the token should be visible to anyone', (done) => {
+        const methods = ['name', 'symbol'].map(k =>
+            async.apply(getGasEstimateAndCall, configuration.neuronInstance[k]));
+        return async.applyEach(
+            methods,
+            configuration.account_addresses[
+                configuration.account_addresses.length - 1
+            ],
+            gasEstimate => 2 * gasEstimate,
+            (err, results) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.strictEqual(results[0], 'Neuron');
+                assert.strictEqual(results[1], 'NRN');
+                return done();
+            },
+        );
+    });
+
+    it('the balance of any address should be visible to any other address', done =>
+        getGasEstimateAndCall(
+            configuration.neuronInstance.balanceOf,
+            configuration.account_addresses[
+                configuration.account_addresses.length - 1
+            ],
+            gasEstimate => 2 * gasEstimate,
+            configuration.account_addresses[0],
+            (err, balance) => {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.strictEqual(balance.toNumber(), 100);
+                return done();
+            },
+        ));
 });
