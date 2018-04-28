5e9bc0251143231c283ac4dedc3cd3d2c0ad957d
Neeraj Kashyap <nkashy1@gmail.com>
2018-04-28 14:08:16 -0700

    First draft of deploy script

--- deploy.js
+++ deploy.js
@@ -1,13 +1,20 @@
+/**
+ * This script allows you to deploy a smart contract to an ethereum-based blockchain of
+ * your choosing. The only requirement is that you have an unlocked account on an
+ * accessible node with enough ether to cover the gas cost of deployment.
+ */
+
 const fs = require('fs');
 const Ganache = require('ganache-core');
 const _ = require('lodash');
+const readline = require('readline');
 const solc = require('solc');
 const Web3 = require('web3');
 const yargs = require('yargs');
 
 yargs
     .usage(
-        '$0 --provider {ipc|http|ws|test} <path-to-provider>',
+        '$0 --provider {ipc|http|ws|test} --provider <path-to-provider> --contract-file <path-to-solidity-file> --contract-name <name-of-conract> --sender-adress <address> [contract-arguments...]',
     )
     .option(
         'provider',
@@ -37,6 +44,13 @@ yargs
             alias: 'n',
             describe: 'Name of contract from contract file that you would like to deploy',
         },
+    )
+    .option(
+        'sender-address',
+        {
+            alias: 's',
+            describe: 'Address of contract creator',
+        },
     );
 
 const {
@@ -44,6 +58,7 @@ const {
     contractName,
     provider,
     providerType,
+    senderAddress,
 } = yargs.argv;
 
 if (providerType !== 'test' && !provider) {
@@ -59,7 +74,7 @@ const contractBytecode = _.get(compiledContract, 'bytecode');
 console.log('Contract compilation complete!');
 
 // Set up web3 client
-console.log(`Creating web3 client with provider type: ${providerType}, provider: ${provider}...`)
+console.log(`Creating web3 client with provider type: ${providerType}, provider: ${provider}...`);
 const web3Providers = {
     ipc: Web3.providers.IpcProvider,
     http: Web3.providers.HttpProvider,
@@ -79,7 +94,70 @@ function makeClient(clientProvider, clientProviderType) {
 const web3 = makeClient(provider, providerType);
 console.log('Web3 client ready!');
 
-// Deploy contract
+/**
+ * Deploy contract:
+ * 1. Estimate gas cost of deployment
+ * 2. Prompt user to either confirm inclusion of twice this estimate or include a custom gas amount
+ * 3. Attempt contract creation
+ * 4. Notify user of success or failure of contract creation
+ *
+ * This part is asynchronous
+ */
 console.log('Deploying contract...');
+web3.eth.estimateGas(
+    { data: contractBytecode },
+    (err, gasEstimate) => {
+        if (err) {
+            throw err;
+        }
 
-// TODO(neeraj): Complete this!
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: `Gas estimate: ${gasEstimate}. Hit ENTER if you would like to allocate that much gas, or enter a custom amount: `,
+        });
+
+        let gasAllocation = gasEstimate;
+
+        rl.on('line', (line) => {
+            const trimmedLine = line.trim();
+
+            if (!trimmedLine) {
+                rl.close();
+            } else {
+                try {
+                    gasAllocation = parseInt(line.trim(), 10);
+                } catch (e) {
+                    console.error(`Error: could not parse ${trimmedLine} as an integer`);
+                    rl.close();
+                    process.exit(1);
+                }
+            }
+        });
+
+        const web3Contract = web3.eth.contract(
+            JSON.parse(_.get(compiledContract, 'interface')),
+        );
+
+        return web3Contract.new(
+            ...yargs.argv._,
+            {
+                from: senderAddress,
+                data: contractBytecode,
+                gas: gasAllocation,
+            },
+            /* eslint-disable consistent-return */
+            (creationErr, contractInstance) => {
+                if (creationErr) {
+                    throw creationErr;
+                }
+
+                if (contractInstance.address) {
+                    console.log(`Contract successfully created: ${contractInstance.address}`);
+                    process.exit(0);
+                }
+            },
+            /* eslint-enable consistent-return */
+        );
+    },
+);
