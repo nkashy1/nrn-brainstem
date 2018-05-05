73042045138d201b5729eb3a97daff5aa05cbb4b
Neeraj Kashyap <nkashy1@gmail.com>
2018-05-05 06:24:40 -0700

    Added tests for compile method
    This yielded a modification of compile
    
    Also added ConsenSys EIP20 implementation for testing (and later
    profiling).

--- compile.js
+++ compile.js
@@ -2,17 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const solc = require('solc');
 
+/**
+ * Compiles the solidity file at the given path, automatically resolving any dependencies along the
+ * way.
+ *
+ * This method is synchronous and may throw an error (for example, if it is not able to find a file
+ * along the given contractPath).
+ *
+ * @param {string} contractPath
+ * @return {Object} Compiled solidity contract as per https://github.com/ethereum/solc-js
+ */
 function compile(contractPath) {
-    const baseDir = path.resolve(path.dirname(contractPath));
-
     function readCallback(importPath) {
         let pathContent;
-        const absoluteImportPath = path.join(baseDir, importPath);
         try {
-            pathContent = fs.readFileSync(absoluteImportPath).toString();
+            pathContent = fs.readFileSync(importPath).toString();
         } catch (e) {
             return {
-                error: `File not found: ${importPath} (resolved to ${absoluteImportPath})`,
+                error: `File not found: ${importPath}`,
             };
         }
 
