b375c722fa57d5ee83374f674f32603cfd4d8ea4
Neeraj Kashyap <neeraj@doc.ai>
2018-07-09 22:23:38 -0700

    Started on Stimuli contract implementation

---
+++ src/stimuli.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.24;
+
+import "./stem.sol";
+
+/**
+ * Stimuli is intended for deployment by data trial principal investigator (PI). There should be
+ * one Stimuli contract per data trial.
+ *
+ * The contract offers the following semantics:
+ * 1. A user, when they consent to enrolling in a data trial, calls the trial's Stimuli contract
+ *    with a secret that was presented to them as part of the consent process. This secret could,
+ *    for example, encode whether or not they match the trial criteria. It could also encode other
+ *    information. The generation of the secret is up to the trial sponsor and the capabilities of
+ *    the doc.ai application. This fires a Stimulus event.
+ * 2. For each enrollment request, the data trial PI can review the request and either accept or
+ *    reject the enrollment. A rejection of the enrollment puts the user on a blacklist whereby
+ *    they can no longer make method calls against the given Stimulus smart contract. An acceptance
+ *    of enrollment triggers a transfer of NRN tokens from the PI's NRN account to that of the user.
+ *    This enrollment bonus is specified as a variable on the Stimuli contract for that trial.
+ *    Whatever the PI's decision, a Response event is fired with a record of the decision and
+ *    NRN payment.
+ * 3. At any time, the PI can reject a user from the data trial, whereby they may no longer make
+ *    method calls against the stimulus contract. Such a rejection fires a Response event.
+ * 4. An accepted user may at any time submit data pertinent to the trial. The data is submitted
+ *    off of this contract, but the user is expected to log their submission by calling the submit
+ *    method of this contract. Submission fires a Stimulus event.
+ * 5. The data trial PI can accept or reject any data submission. Rejection must come with a
+ *    rejection code specifying the reason for rejection. Acceptance is predicated on the PI being
+ *    able to transfer NRN from their account to that of the data trial participant. The amount of
+ *    NRN transferred is determined by the data type (provided with the submission) and the rewards
+ *    are specified in a rewards array publicly visible on the Stimuli contract.
+ *
+ * NOTE: The PI should approve the deployed Stimuli contract to pay out the appropriate amount
+ * of NRN tokens.
+ */
+contract Stimuli {
+    // State variables
+    Stem public nrn;
+    address public pi;
+    uint[5] public rewards;
+
+    // For the values:
+    // 0 - participant has neither been accepted nor rejected into the data trial
+    // 1 - participant has been accepted into the data trial
+    // 2 - participant has been rejected from the data trial
+    mapping(address => uint8) participants;
+
+    // Events
+    // Stimulus types:
+    // 0: enrollment
+    // > 0: data trial-specific collection types
+    // Reward for collection type i is specified by rewards[i]
+    event Stimulus(address indexed _candidate, uint8 indexed _stimulusType, uint _stimulusId);
+    event Response(address indexed _candidate, uint8 indexed _stimulusType, uint stimulusId, bool accepted);
+
+    constructor(address _nrn, uint[5] _rewards) public {
+        nrn = Stem(_nrn);
+        pi = msg.sender;
+
+        uint8 i;
+        for (i = 0; i < 5; i++) {
+            rewards[i] = _rewards[i];
+        }
+    }
+}
---
+++ test/stimuli.js
@@ -0,0 +1,201 @@
+/**
+ * This file contains tests for the Stimuli smart contract found in ../src/stimuli.sol
+ *
+ * You can search for tests for a particular Stimuli contract method by searching for its name in
+ * this file.
+ */
+
+const assert = require('assert');
+const async = require('async');
+const checkBalances = require('./checkBalances.js');
+const compile = require('../compile.js');
+const Ganache = require('ganache-core');
+const getGasEstimateAndCall = require('../getGasEstimateAndCall.js');
+const _ = require('lodash');
+const path = require('path');
+const Web3 = require('web3');
+
+
+/**
+ * Generates contract class and bytecode for use in tests
+ *
+ * @param {string} contractPath - Path to solidity file implementing the contract in question
+ * @param {string} contractName - Name of contract to prepare
+ * @returns {Object} - compiled contract object
+ */
+function contractArtifacts(contractPath, contractName) {
+    const contractFile = path.resolve(__dirname, contractPath);
+    const compilationResult = compile(contractFile);
+    const compiledContract = _.get(compilationResult, ['contracts', `${contractFile}:${contractName}`]);
+    return compiledContract;
+}
+
+
+const stem = contractArtifacts('../src/stem.sol', 'Stem');
+const stimuli = contractArtifacts('../src/stimuli.sol', 'Stimuli');
+
+
+/**
+ * Sets up a web3 client and related objects for use in each test scenario.
+ * Does this by populating the `configuration` object, defined in the scope
+ * of each test case.
+ *
+ * NOTE:
+ * ganache-core is used to simulate an Ethereum node. We use here the default settings, which
+ * provide 10 unlocked accounts on the simulated node. Changes in this default behaviour could
+ * result in our tests breaking.
+ *
+ * The fields it populates in the configuration object are:
+ * 1. accounts - list of (unlocked) account objects available as part of the scenario
+ * 2. account_addresses - list of addresses for each of the accounts (in the same order
+ * as the list of accounts)
+ * 3. Stem - Stem contract object
+ * 4. Stimuli - Stimuli contract object
+ * 5. provider - RPC provider (in this case, we are using a ganache-core Provider object)
+ * 6. web3 - web3 client object
+ *
+ * @param {Object} configuration - Object that should be populated with the test configuration
+ * parameters
+ * @param {callback} done - Callback to be fired with no arguments once setup is complete
+ */
+function setUp(configuration, done) {
+    /* eslint-disable no-param-reassign */
+    configuration.provider = Ganache.provider();
+    configuration.accounts = _.get(configuration.provider, [
+        'manager',
+        'state',
+        'unlocked_accounts',
+    ]);
+    configuration.account_addresses = Object.keys(configuration.accounts);
+    configuration.web3 = new Web3();
+    configuration.web3.setProvider(configuration.provider);
+    configuration.Stem = configuration.web3.eth.contract(
+        JSON.parse(_.get(stem, 'interface')),
+    );
+    configuration.Stimuli = configuration.web3.eth.contract(
+        JSON.parse(_.get(stimuli, 'interface')),
+    );
+
+    const stemBytecode = _.get(stem, 'bytecode');
+    const stimuliBytecode = _.get(stimuli, 'bytecode');
+
+    async.waterfall([
+        // Get gas estimate for Stem contract deployment
+        next => configuration.web3.eth.estimateGas({ data: stemBytecode }, next),
+        // Deply Stem contract
+        (gasEstimate, next) => {
+            // Some of the web3 contract methods (like new) make multiple calls to
+            // the provided callbacks. These callbacks represent different events.
+            // The `callInfo` object tracks the number of calls made to each
+            // callback.
+            const callInfo = {
+                new: 0,
+            };
+
+            return configuration.Stem.new(
+                'Stem',
+                'STM',
+                12000000000000,
+                {
+                    from: configuration.account_addresses[0],
+                    data: stemBytecode,
+                    gas: 2 * gasEstimate,
+                },
+                /* eslint-disable consistent-return */
+                (creationErr, contractInstance) => {
+                    if (creationErr) {
+                        return next(creationErr);
+                    }
+
+                    callInfo.new += 1;
+
+                    if (callInfo.new === 2) {
+                        configuration.stemInstance = contractInstance;
+                        return next(null, contractInstance);
+                    }
+                },
+                /* eslint-enable consistent-return */
+            );
+        },
+        // Get gas estimate for Stimuli contract deployment
+        (stemInstance, next) => {
+            configuration.web3.eth.estimateGas({ data: stimuliBytecode }, (err, gasEstimate) => {
+                if (err) {
+                    return next(err);
+                }
+
+                return next(null, stemInstance, gasEstimate);
+            });
+        },
+        // Deploy Stimuli contract -- mimics Stem deployment
+        (stemInstance, gasEstimate, next) => {
+            const callInfo = {
+                new: 0,
+            };
+
+            return configuration.Stimuli.new(
+                stemInstance.address,
+                [10000000, 5000000, 1000000, 0, 0],
+                {
+                    from: configuration.account_addresses[0],
+                    data: stimuliBytecode,
+                    gas: 2 * gasEstimate,
+                },
+                /* eslint-disable consistent-return */
+                (creationErr, contractInstance) => {
+                    if (creationErr) {
+                        return next(creationErr);
+                    }
+
+                    callInfo.new += 1;
+
+                    if (callInfo.new === 2) {
+                        configuration.stimuliInstance = contractInstance;
+                        return next(null, stemInstance, contractInstance);
+                    }
+                },
+                /* eslint-enable consistent-return */
+            );
+        },
+        // Approve deployed Stimuli contract to spend maximum allowable amount on behalf of deployer
+        (stemInstance, stimuliInstance, next) => getGasEstimateAndCall(
+            stemInstance.approve,
+            configuration.account_addresses[0],
+            gasEstimate => 2 * gasEstimate,
+            stimuliInstance.address,
+            1000000000000,
+            next,
+        ),
+    ], done);
+    /* eslint-enable no-param-reassign */
+}
+
+describe('Stimuli setup', () => {
+    const configuration = {};
+
+    before(done => setUp(configuration, done));
+
+    after(done => configuration.provider.close(done));
+
+    it('should proceed without failure when provided with a valid Stem contract address', () => {});
+
+    it(
+        'should include the Stimuli contract being approved to spend Stem tokens on behalf of its deployer',
+        done => getGasEstimateAndCall(
+            configuration.stemInstance.allowance,
+            configuration.account_addresses[1],
+            gasEstimate => 2 * gasEstimate,
+            configuration.account_addresses[0],
+            configuration.stimuliInstance.address,
+            (err, allowance) => {
+                if (err) {
+                    return done(err);
+                }
+                if (allowance === 0) {
+                    return done(new Error('Expected: non-zero allowance, actual: zero allowance'));
+                }
+                return done();
+            },
+        ),
+    );
+});
